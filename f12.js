af90e07f23ae300dc4f62ee460b0c7fc0ff24942
azzphucdev <phucdz205@gmail.com>
2024-08-09 15:02:48 +0700

    Up

---
+++ f12.js
@@ -0,0 +1,27 @@
+var listBlock = ['&', 'charCodeAt', 'firstChild', 'href', 'join', 'match', '+', '=', 'TK', '<a href=\'/\'>x</a>', 'innerHTML', 'fromCharCode', 'split', 'constructor', 'a', 'div', 'charAt', '', 'toString', 'createElement', 'debugger', '+-a^+6', 'Fingerprint2', 'KT', 'TKK', 'substr', '+-3^+b+-f', '67bc0a0e207df93c810886524577351547e7e0459830003d0b8affc987d15fd7', 'length', 'get', '((function(){var a=1585090455;var b=-1578940101;return 431433+"."+(a+b)})())', '.', 'https?:\/\/', ''];
+(function () {
+    console.log(`
+
+    Source Code By:  AzzPhuc Developer
+    Facebook : https://www.facebook.com/azz.phuc
+    8/8/2024
+    `);
+console.log("%c BLOCKED F12. %c", 'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#ee0047;-webkit-text-fill-color:#ee0047;-webkit-text-stroke: 1px #ee0047;', "font-size:12px;color:#999999;");
+
+    (function block_f12() {
+        try {
+            (function block_f12(data) {
+                if ((listBlock[33] + (data / data))[listBlock[28]] !== 1 || data % 20 === 0) {
+
+                    (function () {})[listBlock[13]](listBlock[20])()
+                } else {
+                    debugger;
+
+                };
+                block_f12(++data)
+            }(0))
+        } catch (e) {
+            setTimeout(block_f12, 500)
+        }
+    })()
+})();
